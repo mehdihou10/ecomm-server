91bba90f406db7c2219704a620ab411dea181c10
mehdi hou <mehdi5juillet@gmail.com>
2024-03-18 02:02:01 +0100

    first commit

---
+++ db.js
@@ -0,0 +1,23 @@
+const postgres = require('postgres');
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const pool = postgres({
+//   host: PGHOST,
+//   database: PGDATABASE,
+//   username: PGUSER,
+//   password: PGPASSWORD,
+//   port: 5432,
+//   ssl: 'require',
+//   connection: {
+//     options: `project=${ENDPOINT_ID}`,
+//   },
+
+    host: "localhost",
+    user: "postgres",
+    password: process.env.PASSWORD,
+    database: "ecomm_final"
+
+});
+
+module.exports = pool;
---
+++ index.js
@@ -0,0 +1,59 @@
+//constants
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+//settings
+
+
+require('dotenv').config();
+
+app.use(cors());
+
+app.use(express.json());
+
+//database connection + start server
+app.get('/',(req,res)=>{
+
+    res.send("<h1>Welcome To This Server!!</h1>");
+})
+
+
+const pool = require('./db');
+
+async function getPgVersion() {
+
+    try{
+
+        const result = await pool`select version()`;
+
+        app.listen(process.env.PORT,()=>{
+
+            console.log('Welcome to mehdi empire');
+        })
+
+    } catch(err){
+
+        throw new Error(err);
+
+    }
+    
+}
+  
+  getPgVersion();
+
+//routes
+
+
+//error handeler
+app.use((err,req,res,next)=>{
+
+    res.json({status: err.status || 'error', code: err.code || 500, message: err.message})
+})
+
+//404 page
+app.use("*",(req,res)=>{
+
+    res.json({message: "route not found"})
+})
