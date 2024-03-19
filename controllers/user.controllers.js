89048d34094331a8a2a73dda20e175dcb029a970
Aissahouar <aissahouar4@gmail.com>
2024-03-20 00:39:49 +0100

    aissa

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,16 +1,56 @@
 const pool = require("../db");
-const httpStatus = require('../utils/http.status');
-const createError = require("../utils/create.error")
+const httpStatus = require("../utils/http.status");
+const createError = require("../utils/create.error");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-const getUsers = async (req, res, next) => {
+const { validationResult } = require("express-validator");
+
+const register = async (req, res) => {
   try {
-    const users = await pool`SELECT * FROM users`;
-    res.status(200).json({ status: httpStatus.SUCCESS ,result:users});
+    const { first_name, last_name, email, password, image } = req.body;
+    const errors = validationResult(req);
+    if (errors) {
+      return res.status(400).send({ message: errors.array() });
+    }
+    const olderUser = await pool`select * from users where email=${email}`;
+    if (olderUser.length > 0) {
+      // const error = createError(httpStatus.FAIL, 400, "user already exists");
+      return res.status(500).json({ error: "user already exists" });
+      // return res.json({error});
+      // return next(error);
+    }
+    const hasedPassword = await bcrypt.hash(password, 10);
+    await pool`insert into users (first_name,last_name,email,password,image) VALUES(${first_name},${last_name},${email},${hasedPassword},${image})`;
+    const token = jwt.sign({ email }, process.env.JWT_KEY, {
+      expiresIn: "3h",
+    });
+    res.status(200).json({ token });
   } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await pool`select * from users where email=${email}`;
+    if (user.length === 0) {
+      return res.status(404).json({ error: "no such user" });
+    }
+    const isValidPassword = await bcrypt.compare(password, user[0].password);
+    if (!isValidPassword) {
+      return res.status(404).json({ error: "password incorrect" });
+    }
+    res.status(200).json({ user });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error });
   }
 };
 
 module.exports = {
-  getUsers,
+  register,
+  login,
 };
