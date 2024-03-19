3f714c2b983a95c3de87ae69a0924753b5c5c3c9
Aissahouar <aissahouar4@gmail.com>
2024-03-19 01:21:52 +0100

    aissa

--- db.js
+++ db.js
@@ -1,23 +1,22 @@
-const postgres = require('postgres');
+const postgres = require("postgres");
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
 const pool = postgres({
-//   host: PGHOST,
-//   database: PGDATABASE,
-//   username: PGUSER,
-//   password: PGPASSWORD,
-//   port: 5432,
-//   ssl: 'require',
-//   connection: {
-//     options: `project=${ENDPOINT_ID}`,
-//   },
+  //   host: PGHOST,
+  //   database: PGDATABASE,
+  //   username: PGUSER,
+  //   password: PGPASSWORD,
+  //   port: 5432,
+  //   ssl: 'require',
+  //   connection: {
+  //     options: `project=${ENDPOINT_ID}`,
+  //   },
 
-    host: "localhost",
-    user: "postgres",
-    password: process.env.PASSWORD,
-    database: "ecomm_final"
-
+  host: "localhost",
+  user: "postgres",
+  password: process.env.PASSWORD,
+  database: process.env.DB,
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,59 +1,50 @@
 //constants
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 //settings
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(cors());
 
 app.use(express.json());
 
 //database connection + start server
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome To This Server!!</h1>");
+});
 
-    res.send("<h1>Welcome To This Server!!</h1>");
-})
-
-
-const pool = require('./db');
+const pool = require("./db");
 
 async function getPgVersion() {
+  try {
+    const result = await pool`select version()`;
 
-    try{
-
-        const result = await pool`select version()`;
-
-        app.listen(process.env.PORT,()=>{
-
-            console.log('Welcome to mehdi empire');
-        })
-
-    } catch(err){
-
-        throw new Error(err);
-
-    }
-    
+    app.listen(process.env.PORT, () => {
+      console.log("Welcome to mehdi empire");
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
 }
-  
-  getPgVersion();
+
+getPgVersion();
 
 //routes
 
-
 //error handeler
-app.use((err,req,res,next)=>{
-
-    res.json({status: err.status || 'error', code: err.code || 500, message: err.message})
-})
+app.use((err, req, res, next) => {
+  res.json({
+    status: err.status || "error",
+    code: err.code || 500,
+    message: err.message,
+  });
+});
 
 //404 page
-app.use("*",(req,res)=>{
-
-    res.json({message: "route not found"})
-})
+app.use("*", (req, res) => {
+  res.json({ message: "route not found" });
+});
