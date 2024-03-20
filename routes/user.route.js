7e09a44a6f4740a5f7581e18ebd886af64dda43a
Aissahouar <aissahouar4@gmail.com>
2024-03-20 01:33:55 +0100

    aissa

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,7 +5,7 @@ const userController = require("../controllers/user.controllers.js");
 const validationScheme = require("../middlewares/verify.user.imput.js");
 //controllers
 
-router.post("/register", validationScheme, userController.register);
-router.post("/login",  userController.login);
+router.post("/register", validationScheme.register, userController.register);
+router.post("/login", validationScheme.login, userController.login);
 
 module.exports = router;
