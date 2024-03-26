3b60613521e9e7f60050996053d50da7366e9899
Aissahouar <aissahouar4@gmail.com>
2024-03-26 01:19:43 +0100

    aissa

--- routes/all.users.route.js
+++ routes/all.users.route.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
+const allUsersController = require("../controllers/all.users.controllers");
 
+const validationScheme = require("../middlewares/verify.user.imput");
+
+router.post("/login", validationScheme.login, allUsersController.login);
 
 module.exports = router;
