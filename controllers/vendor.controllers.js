7c9b58ec8e0789592315a20e32de07c9b8fc0410
Aissahouar <aissahouar6@gmail.com>
2024-03-29 02:31:15 +0100

    added

--- controllers/vendor.controllers.js
+++ controllers/vendor.controllers.js
@@ -1,87 +1,91 @@
-const pool = require('../db');
+const pool = require("../db");
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const httpStatus = require('../utils/http.status');
-const createError = require('../utils/create.error');
+const httpStatus = require("../utils/http.status");
+const createError = require("../utils/create.error");
 
-const {validationResult} = require('express-validator');
+const { validationResult } = require("express-validator");
 
-const generateToken = require('../utils/generate.token');
+const generateToken = require("../utils/generate.token");
 
-const verifyEmail = require('../utils/send.email');
+const verifyEmail = require("../utils/send.email");
 
 //controllers
 
-const addVendor = async(req,res,next)=>{
+const addVendor = async (req, res, next) => {
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    image,
+    phone_number,
+    email_verification,
+  } = req.body;
 
-    const {first_name,last_name,email,password,image,phone_number,email_verification} = req.body;
+  const errors = validationResult(req);
 
+  if (!errors.isEmpty()) {
+    // return res.json({status: httpStatus.FAIL,message: errors.array()})
+    const err = createError(httpStatus.FAIL, 400, errors.array());
 
-    const errors = validationResult(req);
+    return next(err);
+  }
 
-    if(!errors.isEmpty()){
+  const oldVendor = await pool`SELECT id FROM vendor WHERE email=${email}`;
 
-        // return res.json({status: httpStatus.FAIL,message: errors.array()})
-        const err = createError(httpStatus.FAIL,400,errors.array());
+  if (oldVendor.length !== 0) {
+    return res.json({
+      status: httpStatus.FAIL,
+      message: [{ msg: "vendor already registered" }],
+    });
+  }
 
-        return next(err);
-    }
+  const oldUser = await pool`SELECT COUNT(*) FROM users WHERE email=${email}`;
 
-    const oldVendor = await pool`SELECT id FROM vendor WHERE email=${email}`;
+  if (oldUser[0].count > 0) {
+    return res.json({
+      status: httpStatus.FAIL,
+      message: [{ msg: "account already found" }],
+    });
+  }
 
-    if(oldVendor.length !== 0){
+  const hashed_password = await bcrypt.hash(password, 10);
 
-        return res.json({status: httpStatus.FAIL, message: [{msg: "vendor already registered"}]})
-    }
-
-    const oldUser = await pool`SELECT COUNT(*) FROM users WHERE email=${email}`;
-
-    if(oldUser[0].count > 0){
-
-        return res.json({status: httpStatus.FAIL, message: [{msg: "account already found"}]})
-
-    }
-
-    const hashed_password = await bcrypt.hash(password,10);
-
-    if(email_verification){
-
-        const html = `
+  if (email_verification) {
+    const html = `
         <h4>Please copy this code:</h4>
         <code><h4 style='background-color: #eee; padding: 10px 20px;font-size: 20px; width: fit-content'>${email_verification}</h4></code>
-        `
+        `;
 
-        verifyEmail(html,email,"verification code");
+    verifyEmail(html, email, "verification code");
 
-        res.json({status: httpStatus.SUCCESS})
-
-    } else{
-        
-        try{
-
-        await pool`INSERT INTO vendor (first_name,last_name,email,password,image,phone_number) 
+    res.json({ status: httpStatus.SUCCESS });
+  } else {
+    try {
+      await pool`INSERT INTO vendor (first_name,last_name,email,password,image,phone_number) 
         VALUES (${first_name},${last_name},${email},${hashed_password},${image},${phone_number})`;
 
-        const userRes = await pool`SELECT id FROM vendor WHERE email=${email}`;
+      const userRes = await pool`SELECT id FROM vendor WHERE email=${email}`;
 
-        const token = generateToken({id: userRes[0].id,first_name,last_name,email,image,phone_number, type: "vendor"})
+      const token = generateToken({
+        id: userRes[0].id,
+        first_name,
+        last_name,
+        email,
+        image,
+        phone_number,
+        type: "vendor",
+      });
 
-        res.json({status: httpStatus.SUCCESS, token})
-
-
-    } catch(err){
-
-        next(err);
+      res.json({ status: httpStatus.SUCCESS, token });
+    } catch (err) {
+      next(err);
     }
-
-}
-
-
-}
-
+  }
+};
 
 module.exports = {
-
-    addVendor,
-}
+  addVendor,
+};
--- controllers/vendor.dashboard.controllers.js
+++ controllers/vendor.dashboard.controllers.js
@@ -1,7 +1,40 @@
+const pool = require("../db");
+const httpStatus = require("../utils/http.status");
+const createError = require("../utils/create.error");
+// const validationScheme = require("../middlewares/verify.vendor.dash.input");
 
+const getProducts = async (req, res, next) => {
+  const vendorId = req.params.id;
+  try {
+    const prodcuts =
+      await pool`select * from product where vendor_id=${vendorId}`;
+    if (prodcuts.length === 0) {
+      return res.json({ status: httpStatus.SUCCESS, data: "no such products" });
+    }
+    return res.json({ status: httpStatus.SUCCESS, data: prodcuts });
+  } catch (error) {
+    return next(error);
+  }
+};
 
+const addProduct = async (req, res, next) => {
+  const { name, description, vendor_id, category_id, image, price, brand } =
+    req.body;
+  //   const errors = validationScheme(req);
+  //   if (!errors.isEmpty()) {
+  //     const err = createError(httpStatus.FAIL, 400, errors.array());
+  //     return next(err);
+  //   }
 
+  try {
+    await pool`insert into product (name,description,category_id,vendor_id,image,price,brand) values(${name},${description},${category_id},${vendor_id},${image},${price},${brand})`;
+    res.json({ status: httpStatus.SUCCESS });
+  } catch (error) {
+    return next(error);
+  }
+};
 
-
-
-module.exports = {}
+module.exports = {
+  getProducts,
+  addProduct,
+};
