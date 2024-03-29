3cb724d8fb46dcb5e56bc9361dcd294864c2b8c2
Aissahouar <aissahouar6@gmail.com>
2024-03-29 02:33:09 +0100

    aissa

--- routes/vendor.dashboard.router.js
+++ routes/vendor.dashboard.router.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 const vendorDashboardControllers = require('../controllers/vendor.dashboard.controllers');
 
-router.get("/:id", vendorDashboardControllers.getProducts);
-router.post("/add", vendorDashboardControllers.addProduct);
+
 
 module.exports = router;
