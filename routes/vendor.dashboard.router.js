062ce07de82bbd12168e5675beccd9c390de008f
Aissahouar <aissahouar6@gmail.com>
2024-03-29 02:41:00 +0100

    Aissa

--- routes/vendor.dashboard.router.js
+++ routes/vendor.dashboard.router.js
@@ -2,8 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-const vendorDashboardControllers = require('../controllers/vendor.dashboard.controllers');
+const validationDashboard = require("../middlewares/verify.vendor.dashboard");
+const vendorDashboardControllers = require("../controllers/vendor.dashboard.controllers");
 
-
-
+router.get("/:id", validationDashboard, vendorDashboardControllers.getProducts);
+router.post("/add", validationDashboard, vendorDashboardControllers.addProduct);
+router.put(
+  "/:productId",
+  validationDashboard,
+  vendorDashboardControllers.updateProduct
+);
+router.delete("/:productId", vendorDashboardControllers.deleteProduct);
 module.exports = router;
