78fd1bd734399433d417e70c10db5d37a7f0a8a3
mehdi hou <mehdi5juillet@gmail.com>
2024-03-29 02:26:34 +0100

    route

--- routes/vendor.dashboard.router.js
+++ routes/vendor.dashboard.router.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const vendorDashboardControllers = require('../controllers/vendor.dashboard.controllers');
+const validationDashboard = require('../middlewares/verify.vendor.dashboard');
+
+router.put("/:productId",validationDashboard,vendorDashboardControllers.updateProduct);
+router.delete("/:productId",vendorDashboardControllers.deleteProduct);
 
 
 module.exports = router;
