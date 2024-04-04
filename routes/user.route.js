1fac542889effd04c475a2ab335a7592013cb155
Aissahouar <aissahouar6@gmail.com>
2024-04-04 02:23:44 +0100

    aissa

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,6 +6,7 @@ const validationScheme = require("../middlewares/verify.user.imput.js");
 //controllers
 
 router.post("/register", validationScheme.register, userController.register);
+router.put("/update/:userId",validateUpdate,allUsersController.updateUser);
 
 
 module.exports = router;
