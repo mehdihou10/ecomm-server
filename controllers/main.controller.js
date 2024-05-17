1a4abaf912b138344533733f7dc1a5024e38fa7a
mehdi hou <mehdi5juillet@gmail.com>
2024-05-17 22:40:18 +0200

    changes

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,6 +1,7 @@
 const createError = require('../utils/create.error');
 const httpStatus = require('../utils/http.status');
 const pool = require('../db');
+const jwt = require('jsonwebtoken');
 
 
 const getProducts = async (req,res,next)=>{
@@ -60,10 +61,257 @@ const updateViews = async (req,res,next)=>{
     }
 }
 
+const search = async (req,res,next)=>{
+
+    const {text} = req.body;
+
+    if(text.trim() === ""){
+
+        const error = createError(httpStatus.ERROR,400,"Please add a text");
+        return next(error);
+
+    }
+
+    try{
+        
+        const products = await pool`SELECT * FROM product WHERE
+         POSITION(LOWER(${text}) IN LOWER(name)) > 0
+         OR POSITION(LOWER(${text}) IN LOWER(brand)) > 0`;
+
+         if(products.length === 0){
+
+            const error = createError(httpStatus.FAIL,400,"no such product");
+            return next(error)
+         }
+        
+        
+        res.json({status: httpStatus.SUCCESS,products})
+
+    } catch(err){
+        next(err)
+    }
+}
+
+const addToCart = async (req,res,next)=>{
+
+    const headers = req.headers["Authorization"] || req.headers["authorization"];
+
+  const token = headers.split(" ")[1];
+
+  if (!token) {
+    const error = createError(httpStatus.FAIL, 400, "token required");
+    return next(error);
+  }
+
+
+
+  const userId = jwt.decode(token).id;
+
+  const {productId} = req.params;
+  const {qte} = req.body;
+
+  try{
+
+    const count = await pool`SELECT COUNT(*) FROM cart WHERE user_id=${userId} AND product_id=${productId}`;
+    const product_qte = await pool`SELECT qte FROM product WHERE id=${productId}`;
+
+    if(+count[0].count === 0){
+
+        const productQte = +product_qte[0].qte;
+
+        if(+qte <= productQte){
+
+            await pool`INSERT INTO cart(user_id,product_id,qte) VALUES (${userId},${productId},${qte})`;
+            res.json({status: httpStatus.SUCCESS})
+
+        } else{
+
+        res.json({status: httpStatus.FAIL,message: `${productQte === 0 ? 'This product is currently out of stock' : `Max Quantity is: ${productQte}`}`})
+            
+        }
+
+    } else{
+
+        res.json({status: httpStatus.FAIL,message: "Product Already In Cart"})
+    }
+
+
+  } catch(err){
+
+    next(err);
+
+  }
+
+}
+
+const getCart = async (req,res,next)=>{
+
+    const headers = req.headers["Authorization"] || req.headers["authorization"];
+
+  const token = headers.split(" ")[1];
+
+  if (!token) {
+    const error = createError(httpStatus.FAIL, 400, "token required");
+    return next(error);
+  }
+
+  const userId = jwt.decode(token).id;
+
+  const products = await pool`SELECT P.*,C.qte AS product_qte
+                                    FROM cart C,product P
+                                    WHERE C.product_id=P.id AND C.user_id=${userId}`;
+
+
+        res.json({status: httpStatus.SUCCESS,products})  
+
+  try{
+
+  } catch(err){
+    next(err)
+  }
+}
+
+const removeFromCart = async (req,res,next)=>{
+
+    const headers = req.headers["Authorization"] || req.headers["authorization"];
+
+  const token = headers.split(" ")[1];
+
+  if (!token) {
+    const error = createError(httpStatus.FAIL, 400, "token required");
+    return next(error);
+  }
+
+  const userId = jwt.decode(token).id;
+
+  const {productId} = req.params;
+
+  try{
+
+    await pool`DELETE FROM cart WHERE user_id=${userId} AND product_id=${productId}`;
+
+    res.json({status: httpStatus.SUCCESS});
+
+  } catch(err){
+    next(err)
+  }
+
+}
+
+const toggleWishlist = async (req,res,next)=>{
+
+    const headers = req.headers["Authorization"] || req.headers["authorization"];
+
+    const token = headers.split(" ")[1];
+  
+    if (!token) {
+      const error = createError(httpStatus.FAIL, 400, "token required");
+      return next(error);
+    }
+  
+  
+  
+    const userId = jwt.decode(token).id;
+  
+    const {productId} = req.params;
+
+    try{
+
+        const count = await pool`SELECT COUNT(*) FROM wishlist WHERE user_id=${userId} AND product_id=${productId}`;
+
+        if(+count[0].count === 0){
+
+            await pool`INSERT INTO wishlist VALUES(${userId},${productId})`;
+            res.json({status: httpStatus.SUCCESS})
+
+        } else {
+
+            await pool`DELETE FROM wishlist WHERE user_id=${userId} AND product_id=${productId}`;
+            res.json({status: httpStatus.SUCCESS})
+
+        }
+
+    } catch(err){
+        next(err)
+    }
+
+
+}
+
+const isExistsInWishlist = async (req,res,next)=>{
+
+    const headers = req.headers["Authorization"] || req.headers["authorization"];
+
+    const token = headers.split(" ")[1];
+  
+    if (!token) {
+      const error = createError(httpStatus.FAIL, 400, "token required");
+      return next(error);
+    }
+  
+  
+  
+    const userId = jwt.decode(token).id;
+  
+    const {productId} = req.params;
+
+    try{
+
+        const count = await pool`SELECT COUNT(*) FROM wishlist WHERE user_id=${userId} AND product_id=${productId}`;
+
+        if(+count[0].count === 0){
+            res.json({status: httpStatus.SUCCESS,isExists: false})
+        } else{
+            res.json({status: httpStatus.SUCCESS,isExists: true})
+
+        }
+
+    } catch(err){
+        next(err)
+    }
+}
+
+const getWishlist = async(req,res,next)=>{
+
+    const headers = req.headers["Authorization"] || req.headers["authorization"];
+
+    const token = headers.split(" ")[1];
+  
+    if (!token) {
+      const error = createError(httpStatus.FAIL, 400, "token required");
+      return next(error);
+    }
+  
+  
+  
+    const userId = jwt.decode(token).id;
+
+    try{
+
+        const products = await pool`SELECT P.*
+                                    FROM wishlist W,product P
+                                    WHERE W.product_id=P.id AND W.user_id=${userId}`;
+
+
+        res.json({status: httpStatus.SUCCESS,products})                            
+
+    } catch(err){
+        next(err)
+    }
+}
+
+
 module.exports = {
 
     getProducts,
     getProduct,
-    updateViews
+    updateViews,
+    search,
+    addToCart,
+    getCart,
+    removeFromCart,
+    toggleWishlist,
+    isExistsInWishlist,
+    getWishlist
 
 }
--- routes/main.route.js
+++ routes/main.route.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
 const router = express.Router();
-const mainControoler = require('../controllers/main.controller');
+const mainController = require('../controllers/main.controller');
 
-router.get('/products',mainControoler.getProducts);
-router.get("/products/:productName",mainControoler.getProduct);
-router.patch('/products/:productId/add/view',mainControoler.updateViews);
-
+router.get('/products',mainController.getProducts);
+router.get("/products/:productName",mainController.getProduct);
+router.patch('/products/:productId/add/view',mainController.updateViews);
+router.post("/search",mainController.search);
+router.post("/cart/add/:productId",mainController.addToCart);
+router.get("/cart/products",mainController.getCart);
+router.delete("/cart/products/:productId",mainController.removeFromCart);
+router.post("/wishlist/toggle/:productId",mainController.toggleWishlist);
+router.get("/wishlist/verify/:productId",mainController.isExistsInWishlist);
+router.get("/wishlist/products",mainController.getWishlist);
 
 
 
